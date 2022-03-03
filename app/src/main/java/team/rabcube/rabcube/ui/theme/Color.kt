024254a118a1c3399ab4e1ceda7335b666faa878
LuckyStar <luckystar@aether-pro.ru>
2022-03-03 13:19:38 +0300

    Init commit

---
+++ app/src/main/java/team/rabcube/rabcube/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package team.rabcube.rabcube.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Primary = Color(0xFF93E2D8)
+val Secondary = Color(0xFF51AB9F)
+val HeadContainer = Color(0xFF1A7165)
+val BackgroundColor = Color(0xFFC7C7C7)
+val White = Color(0xFFFFFFFF)
